15d8018c4c6cef4c7b4d8b4f004d59ffd602074f
Sanotsch2003 <jonas.mueller.wpk@gmail.com>
2024-12-05 19:23:12 +0100

    continued working on ALU

---
+++ src/Hardware/MiPro-XG-V1/MiPro-XG-V1.srcs/sources_1/new/GPRs.vhd
@@ -0,0 +1,14 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity GPRs is
+    port (
+        
+     );
+end GPRs;
+
+architecture Behavioral of GPRs is
+
+
+end Behavioral;
