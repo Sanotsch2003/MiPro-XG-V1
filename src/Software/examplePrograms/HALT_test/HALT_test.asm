6d6cdc2ef16a70831bd0eea0d34f764149e76a4e
Jonas Müller <116802580+Sanotsch2003@users.noreply.github.com>
2025-01-13 23:31:17 +0100

    Update HALT_test.asm

--- src/Software/examplePrograms/HALT_test/HALT_test.asm
+++ src/Software/examplePrograms/HALT_test/HALT_test.asm
@@ -1,5 +1,6 @@
-define displayControlAddress 0x40000050      ;actual address: 0x40000050
-define displayDataAddress    0x40000054      ;actual address: 0x40000054
+;define constants
+define displayControlAddress 0x40000050      
+define displayDataAddress    0x40000054      
 
 define displayControl        0b00000000111101000010010000100011
 
